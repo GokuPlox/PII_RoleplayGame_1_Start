159f1c3a9320eb8cf38d1af10d150f71dbe88787
GokuPlox <elizabethrominas3@gmail.com>
2023-04-26 20:25:49 -0300

    dwarve

--- src/Library/Dwarves.cs
+++ src/Library/Dwarves.cs
@@ -1,5 +1,26 @@
 
 namespace RoleplayGame
 {
-    
+    public class Dwarve
+    {
+        public int Defense {get;set;}
+        public int Health {get;set;}
+        public int Attack {get;set;}
+        //public Weapon {get;set;}
+        public Dwarve() 
+        {
+            this.Defense=10;
+            this.Health=100;
+            this.Weapon=null;
+            this.Attack=5;
+
+        }
+
+        public void AddArmor(Armor tela)
+        {
+            this.Defense=Defense+tela.getArmor();
+        }
+
+
+    }
 }
