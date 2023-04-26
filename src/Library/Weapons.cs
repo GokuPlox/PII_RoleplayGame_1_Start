738c3de33bae81c9e1d9f2528435f0a31477f384
fedepds <federico.pizarro@correo.ucu.edu.uy>
2023-04-26 20:50:36 -0300

    Weapons 2.0

--- src/Library/Weapons.cs
+++ src/Library/Weapons.cs
@@ -6,9 +6,13 @@ namespace RoleplayGame
         public int Damage {get; set;}
 
         public string Name {get; set;}
-        public Weapons(int damage)
+
+        public int Defense {get; set;}
+        public Weapons(string name, int damage, int defense)
         {
-            this.Damage=damage;
+            this.Damage = damage;
+            this.Defense = defense;
+            this.Name = name;
         }
 
 
