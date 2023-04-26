2ffe1e40c13c358e437ffad0449690d67b7fdaba
enzobueno11 <127800256+enzobueno11@users.noreply.github.com>
2023-04-26 20:17:15 -0300

    cree elves

--- src/Library/Elves.cs
+++ src/Library/Elves.cs
@@ -1,4 +1,18 @@
 namespace RoleplayGame
 {
+    public class Elves
+    {
+          public int attack {get;set;} 
+          public int defense {get;set;}
+
+          public int Health {get;set;}
+
+          public string Weapon {get;set;}
+
+          public string Item {get;set;}
+            
+        
+    }
+       
     
 }
