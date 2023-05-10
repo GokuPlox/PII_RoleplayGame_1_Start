67dcaded51f7b11f8e6949d99092e6072cd31cf0
GokuPlox <elizabethrominas3@gmail.com>
2023-05-10 20:01:49 -0300

    ,

--- src/Library/Armors.cs
+++ src/Library/Armors.cs
@@ -3,15 +3,11 @@ namespace RoleplayGame
     public class Armor
 
     {
-        public int Defense {get;set;}
+        public int Defense {get;}
         public Armor(int defensa)
         {
             this.Defense=defensa;
         }
 
-        public int getArmor()
-        {
-            return this.Defense;
-        }
     }
 }
--- src/Library/Dwarves.cs
+++ src/Library/Dwarves.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RoleplayGame
 {
@@ -6,21 +8,32 @@ namespace RoleplayGame
         public int Defense {get;set;}
         public int Health {get;set;}
         public int Attack {get;set;}
+        public Weapons Weapon {get;set;}
+        public List<object> Items {get;set;}
         //public Weapon {get;set;}
         public Dwarve() 
         {
-            this.Defense=10;
+            Weapons arma = new Weapons("pala", 10, 5, false);
+            this.Defense=0;
             this.Health=100;
-            this.Weapon=null;
-            this.Attack=5;
+            this.Weapon=arma;
+            this.Attack=0;
+            Items.Add(Defense);
+            Items.Add(Weapon);
 
         }
 
         public void AddArmor(Armor tela)
         {
-            this.Defense=Defense+tela.getArmor();
+            this.Defense=Defense+tela.Defense;
+            
         }
 
+        public void AddWeapon(Weapons w)
+        {
+            this.Attack=Attack+w.Damage;
+            Items.Add(w);
+        }
 
     }
 }
